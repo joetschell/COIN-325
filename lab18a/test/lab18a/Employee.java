0b8640cfc481060e2ea3f4fd77e00b51cefa3e27
joetschell <joetschell@hotmail.com>
2015-04-21 11:51:48 -0400

    lab18a

---
+++ lab18a/test/lab18a/Employee.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab18a;
+
+/**
+ *
+ * @author jschell157503
+ */
+public class Employee { 
+     private String mName, mSSN, mBDay, mDiversity; 
+      
+     Employee(String name, String SSN, String BDay, String Diversity) { 
+         mName = name; 
+         mSSN = SSN; 
+         mBDay = BDay; 
+         mDiversity = Diversity; 
+     } 
+      
+     public String getKey() { 
+         return mSSN + mDiversity; 
+     } 
+      
+     public String getBDay() { 
+         return mBDay; 
+     } 
+ } 
